399de593a04ea580b5bcd8c0178a9cf363c32f0f
Marco Edoardo Duma <marcoedoardo.duma@studio.unibo.it>
2021-08-09 23:21:59 +0200

    forgot this

---
+++ src/main/java/petrangola/views/option/OptionViewImpl.java
@@ -0,0 +1,17 @@
+package main.java.petrangola.views.option;
+
+import java.beans.PropertyChangeEvent;
+import main.java.petrangola.views.AbstractViewFX;
+
+public class OptionViewImpl extends AbstractViewFX implements OptionView {
+  
+  public OptionViewImpl() {
+    super();
+  }
+
+  
+  @Override
+  public void propertyChange(PropertyChangeEvent evt) {
+  
+  }
+}
