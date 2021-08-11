336155fb7413b2567d490cafdb1eda796c783222
Marco Edoardo Duma <marcoedoardo.duma@studio.unibo.it>
2021-08-11 14:20:05 +0200

    refactored AbstractViewFX

--- src/main/java/petrangola/views/AbstractViewFX.java
+++ src/main/java/petrangola/views/AbstractViewFX.java
@@ -1,21 +1,38 @@
 package main.java.petrangola.views;
 
-import javafx.scene.Parent;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import main.java.petrangola.utlis.ViewConstants;
 
-public class AbstractViewFX extends Parent {
+public abstract class AbstractViewFX extends Group {
   private final Stage stage;
+  private final Pane layout;
   
-  public AbstractViewFX(Stage stage) {
+  public AbstractViewFX(Stage stage, Pane layout) {
     this.stage = stage;
-    this.stage.setResizable(false);
+    this.layout = layout;
+    
     this.stage.setWidth(ViewConstants.WIDTH.getLength());
     this.stage.setHeight(ViewConstants.HEIGHT.getLength());
+    this.stage.setResizable(false);
+    
+    this.stage.setScene(new Scene(this));
     this.stage.show();
+    this.setNeedsLayout(true);
+    
+    getLayout().setPrefWidth(getRoot().getScene().getWidth());
+    getLayout().setPrefHeight(getRoot().getScene().getHeight());
+    
+    getRoot().getChildren().addAll(getLayout());
   }
   
-  public Stage getStage() {
-    return this.stage;
+  public Group getRoot() {
+    return (Group) this.stage.getScene().getRoot();
+  }
+
+  public Pane getLayout() {
+    return this.layout;
   }
 }
