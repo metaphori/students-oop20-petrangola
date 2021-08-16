a3ac724bf6a02332283a08c7eaf4ec14abb80106
Marco Edoardo Duma <marcoedoardo.duma@studio.unibo.it>
2021-08-16 11:54:58 +0200

    fixed textview

--- src/main/java/petrangola/views/components/textView/SimpleTextView.java
+++ src/main/java/petrangola/views/components/textView/SimpleTextView.java
@@ -1,8 +1,7 @@
 package main.java.petrangola.views.components.textView;
 
-import main.java.petrangola.views.components.ViewNode;
 
-public interface SimpleTextView<R> extends ViewNode {
+public interface SimpleTextView<R> {
   /**
    *
    * @return
--- src/main/java/petrangola/views/components/textView/UsernameTextView.java
+++ src/main/java/petrangola/views/components/textView/UsernameTextView.java
@@ -2,17 +2,19 @@ package main.java.petrangola.views.components.textView;
 
 import javafx.scene.control.TextField;
 import main.java.petrangola.controllers.option.OptionController;
+import main.java.petrangola.views.components.AbstractComponentFX;
 import main.java.petrangola.views.option.commands.UsernameCommand;
 
-public class UsernameTextView extends TextField implements SimpleTextView<String> {
+public class UsernameTextView extends AbstractComponentFX<TextField> implements SimpleTextView<String> {
   private final UsernameCommand command;
   private String text;
   
   public UsernameTextView(final OptionController optionController) {
-    this.setMinWidth(320);
-    this.setWidth(380);
-    this.setMaxWidth(480);
-    this.setStyle("-fx-font-size: 14pt;");
+    super(new TextField());
+    super.get().setMinWidth(320);
+   // super.get().setWidth(380);
+    super.get().setMaxWidth(480);
+    super.get().setStyle("-fx-font-size: 14pt;");
     setListeners();
     
     this.command = new UsernameCommand(this, optionController);
@@ -30,7 +32,7 @@ public class UsernameTextView extends TextField implements SimpleTextView<String
   
   @Override
   public void setListeners() {
-    this.textProperty().addListener((observableValue, s, t1) -> {
+    super.get().textProperty().addListener((observableValue, s, t1) -> {
       setValue(t1);
       this.command.execute();
     });
