976cc295358c1f34f75a1ddc436200781d45e904
Marco Edoardo Duma <marcoedoardo.duma@studio.unibo.it>
2021-08-16 11:45:32 +0200

    updated layoutbuilderImpl
    // TODO: I still have to put some work into this

--- src/main/java/petrangola/views/components/layout/LayoutBuilderImpl.java
+++ src/main/java/petrangola/views/components/layout/LayoutBuilderImpl.java
@@ -1,9 +1,16 @@
 package main.java.petrangola.views.components.layout;
 
 import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import main.java.petrangola.utlis.position.Vertical;
+import main.java.petrangola.views.components.ViewNode;
+
+import java.util.Arrays;
+import java.util.Optional;
 
 public class LayoutBuilderImpl implements LayoutBuilder {
   private Pane pane = null;
@@ -31,6 +38,7 @@ public class LayoutBuilderImpl implements LayoutBuilder {
   
   @Override
   public LayoutBuilder addHBoxForEach(double spacing) {
+    
     this.pane
           .getChildren()
           .stream()
@@ -45,11 +53,37 @@ public class LayoutBuilderImpl implements LayoutBuilder {
     return this.pane;
   }
   
+  public void addToNestedPane(ViewNode viewNode, Vertical position, int depth, Optional<Integer> levelIndex) {
+    Pane pane = getLayout();
+  
+    for (int i = 0; i < depth; i++) {
+      pane = (Pane) pane.getChildren();
+    }
+  
+    pane.getChildren()
+          .stream()
+          .filter(node -> node.getId().equals(position.name()))
+          .map(node -> (Group) node)
+          .findFirst()
+          .ifPresent(value -> value.getChildren().add((Node) viewNode));
+  }
+  
+  private Pane addLayoutPositions(Pane pane) {
+    Arrays.stream(Vertical.values())
+          .forEach(layoutPosition -> {
+            final Group group = new Group();
+            group.setId(layoutPosition.name());
+            pane.getChildren().add(group);
+          });
+    
+    return pane;
+  }
+  
   private void addNode(Pane pane) {
     if (this.pane == null) {
       this.pane = pane;
     } else {
-      this.pane.getChildren().add(pane);
+      this.pane.getChildren().add(addLayoutPositions(pane));
     }
   }
 }
