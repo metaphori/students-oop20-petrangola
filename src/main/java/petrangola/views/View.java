d806838e8ce253b64abedf09d40486d9ea3e5819
Marco Edoardo Duma <marcoedoardo.duma@studio.unibo.it>
2021-08-16 11:55:47 +0200

    propertyChange default method, that I'm not proud of

--- src/main/java/petrangola/views/View.java
+++ src/main/java/petrangola/views/View.java
@@ -2,8 +2,11 @@ package main.java.petrangola.views;
 
 import main.java.petrangola.models.ObservableModel;
 import main.java.petrangola.views.components.ViewNode;
+import main.java.petrangola.views.option.OptionViewImpl;
 
+import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.util.Arrays;
 import java.util.List;
 
 public interface View extends PropertyChangeListener {
@@ -13,5 +16,18 @@ public interface View extends PropertyChangeListener {
    */
   default void addListenerToModel(ObservableModel model) {
     model.addPropertyChangeListener(this);
+  }
+  
+  default void propertyChange(PropertyChangeEvent evt, View view) {
+    Arrays.stream(view.getClass().getDeclaredFields()).forEach(field -> {
+      if (field.getName().equals(evt.getPropertyName())) {
+        try {
+          field.setAccessible(true); // I really don't want to use this but it save me from duplication
+          field.set(this, evt.getNewValue());
+        } catch (IllegalAccessException e) {
+          e.printStackTrace();
+        }
+      }
+    });
   }
 }
