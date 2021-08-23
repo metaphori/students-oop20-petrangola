a19339fd9b852fba4603a4674104c6c0e7864d89
Marco Edoardo Duma <marcoedoardo.duma@studio.unibo.it>
2021-08-23 13:24:30 +0200

    updaterd playercontroller

--- src/main/java/petrangola/controllers/player/PlayerController.java
+++ src/main/java/petrangola/controllers/player/PlayerController.java
@@ -1,27 +1,33 @@
 package main.java.petrangola.controllers.player;
 
 import main.java.petrangola.controllers.Controller;
+import main.java.petrangola.models.cards.Cards;
+import main.java.petrangola.models.game.Game;
+import main.java.petrangola.models.player.Player;
+import main.java.petrangola.models.player.PlayerDetail;
 
 public interface PlayerController extends Controller {
   /**
-   *
+   * @param player
+   * @param boardCards
+   * @param ownCards
    */
-  void exchangeCards();
+  void exchangeCards(final Player player, final Cards boardCards, final Cards ownCards);
   
   /**
-   *
+   * @param game
    */
-  void knock();
+  void knock(final Game game);
   
   /**
-   *
+   * @param playerDetail
    */
-  void looseLife();
+  void looseLife(final PlayerDetail playerDetail);
   
   /**
-   *
+   * @param playerDetail
+   * @return
    */
-  boolean checkIfStillAlive();
-  
+  boolean checkIfStillAlive(final PlayerDetail playerDetail);
   
 }
--- src/main/java/petrangola/controllers/player/PlayerControllerImpl.java
+++ src/main/java/petrangola/controllers/player/PlayerControllerImpl.java
@@ -1,4 +1,30 @@
 package main.java.petrangola.controllers.player;
 
-public class PlayerControllerImpl {
+import main.java.petrangola.models.cards.Cards;
+import main.java.petrangola.models.game.Game;
+import main.java.petrangola.models.player.Player;
+import main.java.petrangola.models.player.PlayerDetail;
+
+public class PlayerControllerImpl implements PlayerController {
+  
+  
+  @Override
+  public void exchangeCards(final Player player, final Cards boardCards, final Cards ownCards) {
+    player.exchange(boardCards, ownCards);
+  }
+  
+  @Override
+  public void knock(final Game game) {
+    game.setKnockerCount(game.getKnockerCount() + 1);
+  }
+  
+  @Override
+  public void looseLife(final PlayerDetail playerDetail) {
+    playerDetail.takeLife();
+  }
+  
+  @Override
+  public boolean checkIfStillAlive(final PlayerDetail playerDetail) {
+    return playerDetail.getPlayerLives() > 0;
+  }
 }
