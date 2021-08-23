b19dede1e99a92ee37cae385b6eea08a313d7a63
Marco Edoardo Duma <marcoedoardo.duma@studio.unibo.it>
2021-08-23 13:32:14 +0200

    finally a  simple implementation of the layout builder

--- src/main/java/petrangola/views/components/layout/LayoutBuilder.java
+++ src/main/java/petrangola/views/components/layout/LayoutBuilder.java
@@ -1,30 +1,36 @@
 package main.java.petrangola.views.components.layout;
 
+
+import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.layout.Pane;
+import main.java.petrangola.utlis.Pair;
+import main.java.petrangola.utlis.position.Horizontal;
+import main.java.petrangola.utlis.position.Vertical;
+import main.java.petrangola.views.ViewNodeFactory;
+import main.java.petrangola.views.game.GameIds;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
 
 public interface LayoutBuilder {
+  Map<Pos, Pair<Vertical, Horizontal>> getPositionsMap();
+  
+  LayoutBuilder addHBox(List<Pair<? extends Node, String>> childNodes);
+  
+  LayoutBuilder addVBox(List<Pair<? extends Node, String>> childNodes);
+  
+  LayoutBuilder addGroup(List<Pair<? extends Node, String>> childNodes);
+  
+  LayoutBuilder addNodeById(String Id, Function<ViewNodeFactory, Node> viewNode);
   
   /**
-   *
-   * @param spacing
-   * @param isCentered
    * @return
    */
-  LayoutBuilder addVBox(double spacing, boolean isCentered);
-  
-  /**
-   *
-   * @param spacing
-   * @return
-   */
-  LayoutBuilder addHBox(double spacing);
-  
-  /**
-   *
-   * @param spacing
-   * @return
-   */
-  LayoutBuilder addHBoxForEach(double spacing);
-  
   Pane getLayout();
+  
+  default Pair<? extends Node, String> addPair(Node node, String Id) {
+    return new Pair<>(node, Id);
+  }
 }
--- src/main/java/petrangola/views/components/layout/LayoutBuilderImpl.java
+++ src/main/java/petrangola/views/components/layout/LayoutBuilderImpl.java
@@ -1,89 +1,152 @@
 package main.java.petrangola.views.components.layout;
 
+import javafx.collections.ObservableList;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Node;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
+import javafx.scene.Parent;
+import javafx.scene.layout.*;
+import main.java.petrangola.utlis.Delimiter;
+import main.java.petrangola.utlis.Pair;
+import main.java.petrangola.utlis.position.Horizontal;
+import main.java.petrangola.utlis.position.Position;
 import main.java.petrangola.utlis.position.Vertical;
-import main.java.petrangola.views.components.ViewNode;
+import main.java.petrangola.views.ViewNodeFactory;
 
-import java.util.Arrays;
-import java.util.Optional;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class LayoutBuilderImpl implements LayoutBuilder {
-  private Pane pane = null;
   
-  public LayoutBuilderImpl() { }
+  private final Map<Pos, Pair<Vertical, Horizontal>> positionsMap = new EnumMap<>(Pos.class);
+  private final Pane layout;
+  private final Position[] layoutPosition;
+  
+  
+  public LayoutBuilderImpl(Pane layout, Position[] layoutPositions) {
+    this.layout = layout;
+    this.layoutPosition = layoutPositions;
+    
+    initPositionsMap();
+    initLayout();
+  }
+  
+  public Map<Pos, Pair<Vertical, Horizontal>> getPositionsMap() {
+    return positionsMap;
+  }
   
   @Override
-  public LayoutBuilder addVBox(double spacing, boolean isCentered) {
-    final VBox vBox = new VBox(spacing);
+  public LayoutBuilder addHBox(List<Pair<? extends Node, String>> childNodes) {
+    final HBox hBox = new HBox();
+  
+    addToLayout(hBox, getChildNodes(childNodes));
     
-    if (isCentered) {
-      vBox.setAlignment(Pos.CENTER);
+    return this;
+  }
+  
+  @Override
+  public LayoutBuilder addVBox(List<Pair<? extends Node, String>> childNodes) {
+    final VBox vBox = new VBox();
+    
+    addToLayout(vBox, getChildNodes(childNodes));
+    
+    return this;
+  }
+  
+  @Override
+  public LayoutBuilder addGroup(List<Pair<? extends Node, String>> childNodes) {
+    final Pane pane = new Pane();
+    final Group group = new Group();
+    
+    pane.getChildren().add(setGrow(getLayout(), group));
+    
+    addToLayout(pane, getChildNodes(childNodes));
+    
+    return this;
+  }
+  
+  @Override
+  public LayoutBuilder addNodeById(String Id, Function<ViewNodeFactory, Node> viewNode) {
+    return this;
+  }
+  
+  private void addToLayout(Pane pane, List<? extends Node> childNodes) {
+    pane.getChildren().addAll(childNodes);
+    getLayout().getChildren().add(pane);
+  }
+  
+  private Node setGrow(Pane pane, Node node) {
+    if (VBox.class.isAssignableFrom(pane.getClass())) {
+      VBox.setVgrow(node, Priority.ALWAYS);
+    }
+  
+    if (HBox.class.isAssignableFrom(pane.getClass())) {
+      HBox.setHgrow(node, Priority.ALWAYS);
     }
     
-    this.addNode(vBox);
-    
-    return this;
+    return node;
   }
   
-  @Override
-  public LayoutBuilder addHBox(double spacing) {
-    this.addNode(new HBox(spacing));
-    return this;
+  private List<? extends Node> getChildNodes(List<Pair<? extends Node, String>> childNodes) {
+    return childNodes.stream().map(pair -> {
+      final Node node = pair.getX();
+      node.setId(pair.getY());
+      
+      return node;
+    }).collect(Collectors.toList());
   }
   
-  @Override
-  public LayoutBuilder addHBoxForEach(double spacing) {
+  private void initLayout() {
+    ObservableList<Node> childNode = this.layout.getChildren();
     
-    this.pane
-          .getChildren()
-          .stream()
-          .map(node -> (Pane) node)
-          .forEachOrdered(pane -> pane.getChildren().add(new HBox(spacing)));
-    
-    return this;
+    Arrays.stream(this.layoutPosition)
+          .forEachOrdered(position -> {
+            final Region region = new Region();
+            
+            region.setId(Horizontal.CENTER.name().concat(Delimiter.UNDERSCORE.getText()).concat(position.name()));
+            
+            HBox.setHgrow(region, Priority.ALWAYS);
+            
+            childNode.add(region);
+          });
   }
+  
+  private void initPositionsMap() {
+    Arrays.stream(Vertical.values())
+          .forEach(vertical -> Arrays.stream(Horizontal.values())
+                                     .forEach(horizontal -> {
+                                       String posString = vertical.name().concat(Delimiter.UNDERSCORE.getText()).concat(horizontal.name());
+                  
+                                       if (vertical.name().equals(horizontal.name())) {
+                                         posString = vertical.name();
+                    
+                                       }
+                  
+                                       final Pos pos = Pos.valueOf(posString);
+                  
+                                       positionsMap.put(pos, new Pair<>(vertical, horizontal));
+                                     }));
+  }
+  
+  public Pos getPos(final Pair<Vertical, Horizontal> pair) {
+    Optional<Map.Entry<Pos, Pair<Vertical, Horizontal>>> optionalEntry = this.positionsMap
+                                                                               .entrySet()
+                                                                               .stream()
+                                                                               .filter(entry -> entry.getValue().equals(pair))
+                                                                               .findFirst();
+    
+    if (optionalEntry.isEmpty()) {
+      throw new IllegalStateException();
+    }
+    
+    return optionalEntry.get().getKey();
+  }
+  
+  
   
   @Override
   public Pane getLayout() {
-    return this.pane;
-  }
-  
-  public void addToNestedPane(ViewNode viewNode, Vertical position, int depth, Optional<Integer> levelIndex) {
-    Pane pane = getLayout();
-  
-    for (int i = 0; i < depth; i++) {
-      pane = (Pane) pane.getChildren();
-    }
-  
-    pane.getChildren()
-          .stream()
-          .filter(node -> node.getId().equals(position.name()))
-          .map(node -> (Group) node)
-          .findFirst()
-          .ifPresent(value -> value.getChildren().add((Node) viewNode));
-  }
-  
-  private Pane addLayoutPositions(Pane pane) {
-    Arrays.stream(Vertical.values())
-          .forEach(layoutPosition -> {
-            final Group group = new Group();
-            group.setId(layoutPosition.name());
-            pane.getChildren().add(group);
-          });
-    
-    return pane;
-  }
-  
-  private void addNode(Pane pane) {
-    if (this.pane == null) {
-      this.pane = pane;
-    } else {
-      this.pane.getChildren().add(addLayoutPositions(pane));
-    }
+    return this.layout;
   }
 }
