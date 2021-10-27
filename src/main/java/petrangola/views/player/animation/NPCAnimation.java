72376b8be9198047b206ec56e76310af3bf2b3a4
Marco Edoardo Duma <marcoedoardo.duma@studio.unibo.it>
2021-10-27 17:27:40 +0200

    delete useless Classes and interfaes

--- src/main/java/petrangola/views/player/animation/NPCAnimation.java
+++
@@ -1,9 +0,0 @@
-package main.java.petrangola.views.player.animation;
-
-public interface NPCAnimation extends PlayerAnimation {
-  
-  NPCAnimation exchange();
-  
-  NPCAnimation firstExchange();
-  
-}
