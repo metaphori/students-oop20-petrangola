e5345e005d6ed9146fb42fe5eeefa737ca4e64eb
Roberto Casadei <robyonrails@gmail.com>
2021-11-26 18:59:04 +0100

    OOP machinery

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module petrangola {
-    requires javafx.controls;
-    requires javafx.graphics;
-    requires java.desktop;
-    requires eventbus;
-    requires combinatoricslib3;
-
-    exports petrangola;
-}
